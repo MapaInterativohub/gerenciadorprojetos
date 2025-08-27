5987d0af6547de64572e319493d406ebb35e5085
ismailergregorio <ismailergregorio6@gmail.com>
2025-08-26 21:25:39 -0300

    .ajuste entidade projetos

--- gerenciadorprojetos/src/main/java/br/edu/suauniversidade/fabrica/gerenciadorprojetos/Service/ProjetoService/ServicePutProjetos.java
+++ gerenciadorprojetos/src/main/java/br/edu/suauniversidade/fabrica/gerenciadorprojetos/Service/ProjetoService/ServicePutProjetos.java
@@ -4,17 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import javax.management.RuntimeErrorException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.edu.suauniversidade.fabrica.gerenciadorprojetos.DTO.ProjetoDTO.dtoProjetoAtulizacaoInfomacao;
-import br.edu.suauniversidade.fabrica.gerenciadorprojetos.DTO.ProjetoDTO.dtoProjetoResp;
 import br.edu.suauniversidade.fabrica.gerenciadorprojetos.Model.ClassAlunos;
+import br.edu.suauniversidade.fabrica.gerenciadorprojetos.Model.ClassGestores;
 import br.edu.suauniversidade.fabrica.gerenciadorprojetos.Model.ClassProjetos;
 import br.edu.suauniversidade.fabrica.gerenciadorprojetos.Repository.RepositoryAlunos;
+import br.edu.suauniversidade.fabrica.gerenciadorprojetos.Repository.RepositoryGestores;
 import br.edu.suauniversidade.fabrica.gerenciadorprojetos.Repository.RepositoryProjetos;
 
 @Service
@@ -25,44 +24,74 @@ public class ServicePutProjetos {
   @Autowired
   RepositoryAlunos repositoryAlunos;
 
+  @Autowired
+  RepositoryGestores repositoryGestores;
+
   public ResponseEntity<dtoProjetoAtulizacaoInfomacao> putProjeto(String identificador,
       dtoProjetoAtulizacaoInfomacao objDto) {
 
     Optional<ClassProjetos> projeto = repositoryProjetos.findByCodigoProjeto(identificador);
 
-    if (projeto.isEmpty()) {
-      System.out.println("Class Não existe");
-      return ResponseEntity.badRequest().build();
-    }
+    ClassProjetos listaAlunosEgestores = projeto.get();
+
+    List<String> raS = new ArrayList<>();
+    List<String> codigosGestores = new ArrayList<>();
 
     if (objDto.getAlunosParticipantesDoProjeto() != null) {
-      ClassProjetos apagarLista = projeto.get();
-      apagarLista.getAlunosParticipantesDoProjeto().clear();
+      // Apaga o projeto existente na entidade aluno para inserir um novo
+      for (ClassAlunos projetoDoAluno : listaAlunosEgestores.getAlunosParticipantesDoProjeto()) {
+        projetoDoAluno.setProjetoSelecionado(null);
+        repositoryAlunos.save(projetoDoAluno);
+      }
 
       for (String ra : objDto.getAlunosParticipantesDoProjeto()) {
-        // Verifica se Aluno existe
         Optional<ClassAlunos> alunoExiste = repositoryAlunos.findByRa(ra);
+
         if (alunoExiste.isPresent()) {
           ClassAlunos projetoAluno = alunoExiste.get();
-          projetoAluno.setProjetoSelecionado(apagarLista);
+          projetoAluno.setProjetoSelecionado(listaAlunosEgestores);
           repositoryAlunos.save(projetoAluno);
-
-          System.out.println(ra + " existe");
-          System.out.println(projetoAluno.getProjetoSelecionado() + " agora tem "+ projetoAluno.getRa());
+          raS.add(projetoAluno.getRa());
         } else {
-          System.out.println(ra + " não existe");
+          return ResponseEntity.badRequest().build();
         }
       }
-    } else {
-      System.out.println("Erro na Verificação de alunos");
+    }
+
+    if (objDto.getProfesorOrientador() != null) {
+      // Apaga o projeto existente na entidade aluno para inserir um novo
+      for (ClassGestores projetoDoGestor : listaAlunosEgestores.getProfesorOrientador()) {
+        projetoDoGestor.setProjetos(null);
+        repositoryGestores.save(projetoDoGestor);
+      }
+
+      for (String Gestor : objDto.getProfesorOrientador()) {
+        Optional<ClassGestores> gestorExistente = repositoryGestores.findByCodigoGestor(Gestor);
+
+        if (gestorExistente.isPresent()) {
+          ClassGestores projetoGestor = gestorExistente.get();
+          projetoGestor.setProjetos(projetoGestor.getProjetos());
+          repositoryGestores.save(projetoGestor);
+          codigosGestores.add(projetoGestor.getCodigoGestor());
+        } else {
+          return ResponseEntity.badRequest().build();
+        }
+      }
+    }else{
       return ResponseEntity.badRequest().build();
     }
 
     dtoProjetoAtulizacaoInfomacao respostaDTO = new dtoProjetoAtulizacaoInfomacao();
 
     respostaDTO.setNomeDoProjeto(objDto.getNomeDoProjeto());
-    respostaDTO.setAlunosParticipantesDoProjeto(objDto.getAlunosParticipantesDoProjeto());
-
+    respostaDTO.setDescricaoDoProjeto(objDto.getDescricaoDoProjeto());
+    respostaDTO.setAreaDeConhecimento(objDto.getAreaDeConhecimento());
+    respostaDTO.setDataDeInicioDoProjeto(objDto.getDataDeInicioDoProjeto());
+    respostaDTO.setDataDoFimDoProjeto(objDto.getDataDoFimDoProjeto());
+    respostaDTO.setAlunosParticipantesDoProjeto(raS);
+    respostaDTO.setProfesorOrientador(codigosGestores);
+    respostaDTO.setLinkGit(objDto.getLinkGit());
+    respostaDTO.setLinkImage(objDto.getLinkImage());
     return ResponseEntity.ok(respostaDTO);
   }
 }
